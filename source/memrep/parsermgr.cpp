8030c8e0652befc524f09c2e8d5df6aa896b5d0c
Matt Phillips <mattphillipsphd@yahoo.com>
2013-12-27 11:54:16 -0500

    laptop Fri, Dec 27, 2013 11:54:16 AM

---
+++ source/memrep/parsermgr.cpp
@@ -0,0 +1,213 @@
+#include "parsermgr.h"
+
+ParserMgr::ParserMgr()
+{
+}
+ParserMgr::~ParserMgr()
+{
+    ClearModels();
+}
+
+void ParserMgr::AddExpression(const std::string& exprn)
+{
+    const std::string& old_exprn = _parser.GetExpr();
+    _parser.SetExpr(old_exprn.empty() ? "" : old_exprn + ", "
+                     + exprn );
+}
+void ParserMgr::AddModel(ParamModel* model, double* data)
+{
+    if (!data) data = new double[model->NumPars()];
+    _models.push_back( std::make_pair(model, data) );
+}
+void ParserMgr::AssignInputs()
+{
+    for (auto& it : _models)
+    {
+        ParamModel* model = it.first;
+        const size_t num_pars = model->NumPars();
+        for (size_t i=0; i<num_pars; ++i)
+        {
+            const std::string& value = model->Value(i);
+            SetInput(model, i, value);
+        }
+    }
+}
+void ParserMgr::ClearModels()
+{
+    for (auto it : _models)
+        delete it.second;
+    _models.clear();
+}
+
+double* ParserMgr::Data(const ParamModel* model)
+{
+    auto it = std::find_if(_models.cbegin(), _models.cend(),
+                        [&](std::pair<ParamModel*,double*> p)
+    {
+            return p.first == model;
+    });
+    return it->second;
+}
+
+void ParserMgr::DefineVars()
+{
+    try
+    {
+        for (auto& it : _models)
+        {
+            ParamModel* model = it.first;
+            double* data = it.second;
+            const size_t num_pars = model->NumPars();
+            for (size_t i=0; i<num_pars; ++i)
+            {
+                const std::string& key = model->ShortKey(i),
+                        & value = model->Value(i);
+                data[i] = atof(value.c_str()); // ###
+
+                //Set up the parsers
+                _parser.DefineVar(key, &data[i]);
+                _parserResult.DefineVar(key, &data[i]);
+                for (auto& itp : _parserConds)
+                    itp.DefineVar(key, &data[i]);
+
+                //Assign the source of the data--i.e. attach an input if needed
+                SetInput(model, i, value);
+            }
+        }
+    }
+    catch (mu::ParserError& e)
+    {
+        std::cerr << e.GetMsg() << std::endl;
+        qDebug() << e.GetMsg().c_str();
+    }
+}
+void ParserMgr::InitParsers()
+{
+    try
+    {
+        VecStr initializations, expressions;
+        for (auto& it : _models)
+        {
+            ParamModel* model = it.first;
+            VecStr model_inits = model->Initializations(),
+                    model_exprns = model->Expressions();
+            initializations.insert(initializations.end(),
+                                   model_inits.begin(),
+                                   model_inits.end());
+            expressions.insert(expressions.end(),
+                                   model_exprns.begin(),
+                                   model_exprns.end());
+        }
+
+        for (const auto& it : initializations)
+        {
+            SetExpression(it);
+            _parser.Eval();
+        }
+
+        SetExpression(expressions);
+            //Note that the expressions are not actually evaluated
+    }
+    catch (mu::ParserError& e)
+    {
+        std::cerr << e.GetMsg() << std::endl;
+    }
+}
+
+void ParserMgr::InputEval(int idx)
+{
+    if (idx==-1)
+    {
+        for (auto& it : _inputs)
+            it.NextInput();
+    }
+    else
+        _inputs[idx].NextInput();
+}
+void ParserMgr::ParserCondEval()
+{
+    int num_conds = (int)_conditions->NumPars();
+    for (int k=0; k<num_conds; ++k)
+    {
+        if (_parserConds.at(k).Eval())
+        {
+            VecStr cond_exprns = _conditions->Expressions(k);
+            for (const auto& it : cond_exprns)
+            {
+                _parserResult.SetExpr(it);
+                _parserResult.Eval();
+            }
+        }
+    }
+}
+void ParserMgr::ParserEval()
+{
+    try
+    {
+        _parser.Eval();
+        InputEval();
+    }
+    catch (mu::ParserError& e)
+    {
+        std::cerr << e.GetMsg() << std::endl;
+    }
+}
+
+void ParserMgr::SetConditions()
+{
+    int num_conds = (int)_conditions->NumPars();
+    for (int k=0; k<num_conds; ++k)
+    {
+        std::string expr = _conditions->Condition(k);
+        _parserConds[k].SetExpr(expr);
+    }
+}
+void ParserMgr::SetCondModel(ConditionModel* conditions)
+{
+    _conditions = conditions;
+    _parserConds = std::vector<mu::Parser>( conditions->NumPars() );
+    SetConditions();
+}
+void ParserMgr::SetExpression(const std::string& exprn)
+{
+    _parser.SetExpr(exprn);
+}
+void ParserMgr::SetExpression(const VecStr& exprns)
+{
+    _parser.SetExpr("");
+    for (const auto& it: exprns)
+        AddExpression(it);
+}
+void ParserMgr::SetInput(const ParamModel* model, size_t i, const std::string& type_str)
+{
+    Input::TYPE type = Input::Type(type_str);
+    double* data = Data(model);
+    switch (type)
+    {
+        case Input::UNI_RAND:
+        case Input::GAMMA_RAND:
+        case Input::NORM_RAND:
+        {
+            Input input(&data[i]);
+            input.GenerateInput(type);
+            _inputs.push_back(input);
+            break;
+        }
+        case Input::TXT_FILE:
+        {
+            Input input(&data[i]);
+            std::string file_name = model->Value(i);
+            std::remove_if(file_name.begin(), file_name.end(), [&](std::string::value_type c)
+            {
+                return c=='"';
+            });
+            input.LoadInput(file_name);
+            _inputs.push_back(input);
+            break;
+        }
+        case Input::USER:
+            break;
+        case Input::UNKNOWN:
+            throw("Error, ParserMgr::SetInput");
+    }
+}
---
+++ source/memrep/parsermgr.h
@@ -0,0 +1,48 @@
+#ifndef PARSERMGR_H
+#define PARSERMGR_H
+
+#include <exception>
+
+#include <QDebug>
+
+#include <muParser.h>
+
+#include "../memrep/input.h"
+#include "../models/conditionmodel.h"
+#include "../models/parammodel.h"
+
+typedef std::vector<std::string> VecStr;
+class ParserMgr
+{
+    public:
+        ParserMgr();
+        ~ParserMgr();
+
+        void AddCondModel(ConditionModel* model);
+        void AddExpression(const std::string& exprn);
+        void AddModel(ParamModel* model, double* data = nullptr);
+        void AssignInputs();
+            //Assign the source of the data for the variables
+        void ClearModels();
+        double* Data(const ParamModel* model);
+        void DefineVars();
+        void InitParsers();
+        void InputEval(int idx = -1);
+        void ParserCondEval();
+        void ParserEval();
+        void SetCondModel(ConditionModel* conditions);
+        void SetConditions();
+        void SetExpression(const std::string& exprn);
+        void SetExpression(const VecStr& exprns);
+        void SetInput(const ParamModel* model, size_t i, const std::string& type_str);
+
+    private:
+        ConditionModel* _conditions;
+        std::vector<Input> _inputs;
+        std::vector< std::pair<ParamModel*, double*> > _models;
+        mu::Parser _parser, _parserResult;
+            //_parserResult is for when conditions get satisfied
+        std::vector<mu::Parser> _parserConds;
+};
+
+#endif // PARSERMGR_H
