3481549c8671b2eaeea37d0b98672986ae5db736
Matt Phillips <mattphillipsphd@yahoo.com>
2015-06-30 14:48:06 -0400

    trying to initialize

--- source/globals/scopetracker.cpp
+++
@@ -1,9 +0,0 @@
-#include "scopetracker.h"
-
-std::mutex ScopeTracker::_mutex;
-std::map<std::thread::id, int> ScopeTracker::_tabCts;
-
-void ScopeTracker::Add(const std::string& mesg)
-{
-    _add += ", " + mesg;
-}
--- source/models/parammodel.cpp
+++
@@ -1,6 +0,0 @@
-#include "parammodel.h"
-
-ParamModel::ParamModel(QObject *parent, const std::string& name) :
-    ParamModelBase(parent, name)
-{
-}
